510ab363113dfe47aa653d7b135a30774767ffd3
Marie Metay <mariemetay.pro@gmail.com>
2024-02-23 17:44:53 +0100

    Add models for olympic & participation

--- src/app/core/models/Participation.ts
+++ src/app/core/models/Participation.ts
@@ -9,3 +9,12 @@ example of participation:
     athleteCount: 372
 }
 */
+export class Participation {
+    constructor(
+        public id: number,
+        public year: number,
+        public city: string,
+        public medalsCount: number,
+        public athleteCount: number
+    ) {}
+}
