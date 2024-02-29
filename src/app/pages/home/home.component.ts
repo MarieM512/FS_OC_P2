db904ae647f0d833d69774e844b3b2fbbf4989f1
Marie Metay <mariemetay.pro@gmail.com>
2024-02-29 19:40:46 +0100

    WIP link chart // data

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ChartConfiguration, ChartType } from 'chart.js';
-import { Observable, of } from 'rxjs';
+import { Observable, filter, map, of, tap } from 'rxjs';
 import { Olympic } from 'src/app/core/models/Olympic';
+import { Participation } from 'src/app/core/models/Participation';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 
 @Component({
@@ -10,24 +11,86 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  public olympics$: Observable<Olympic[]> = of([]);
-
-  constructor(private olympicService: OlympicService) {}
-
-  ngOnInit(): void {
-    this.olympics$ = this.olympicService.getOlympics();
-  }
+  public olympics$: Observable<Olympic[]> = of([])
+  // public countries: String[] = []
+  public medalsNumber: number[] = []
 
   public pieChartOptions: ChartConfiguration['options'] = {
     responsive: true,
   };
   public pieChartType: ChartType = 'pie';
-  public pieChartData = {
-    labels: ['Sales Q1', 'Sales Q2', 'Sales Q3', 'Sales Q4'],
+
+  public countries$: Observable<String[]> = of([])
+
+  
+
+  constructor(private olympicService: OlympicService) {}
+
+  ngOnInit(): void {
+    this.olympics$ = this.olympicService.getOlympics();
+    this.olympics$.pipe(
+      map(data => data.map(item => new Olympic(item.id, item.country, item.participations)))
+    )
+
+    
+
+    // this.olympics$.subscribe(item => {
+    //   item.forEach(value => {
+    //     this.countries.push(value.country)
+    //   })
+    //   item.forEach(value => {
+    //     let medals = 0
+    //     value.participations.forEach(participation => {
+    //       medals += participation.medalsCount
+    //     })
+    //     this.medalsNumber.push(medals)
+    //   })
+    //   console.log(this.medalsNumber)
+    // })
+    // console.log(this.medalsNumber)
+
+    this.olympics$.subscribe(item => {
+      let countries: String[] = []
+      item.forEach(value => {
+        countries.push(value.country)
+      })
+      this.countries$ = of(countries)
+
+      item.forEach(value => {
+        let medals = 0
+        value.participations.forEach(participation => {
+          medals += participation.medalsCount
+        })
+        this.medalsNumber.push(medals)
+      })
+      // console.log(this.countries$)
+      console.log(this.medalsNumber)
+      this.pieChartData.datasets.forEach(value => {
+        value.data = this.medalsNumber
+      })
+    })
+    // console.log(this.medalsNumber)
+  }
+
+  // getLabels(): String[] {
+  //   return this.countries
+  // }
+
+  getMedals(): number[] {
+    return this.medalsNumber
+  }
+
+  public pieChartData = 
+  {
+    // labels: ["Italy", "France", "Germany", "Spain", "United State"],
+    // labels: ,
     datasets: [{
-      data: [120, 150, 180, 90],
-      backgroundColor: ['red', 'orange', 'yellow', 'green'],
-      hoverBackgroundColor: ['darkred', 'darkorange', 'darkyellow', 'darkgreen']
+      data: [1, 2, 3, 4, 5],
+      // data: this.getMedals(),
+      backgroundColor: ['red', 'orange', 'yellow', 'green', 'brown'],
+      hoverBackgroundColor: ['darkred', 'darkorange', 'darkyellow', 'darkgreen', 'darkbrown']
     }]
   };
+
+  
 }
