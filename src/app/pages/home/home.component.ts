1d2292df99c31fb8133814bd0683e62d1959f79d
Marie Metay <mariemetay.pro@gmail.com>
2024-03-01 19:21:28 +0100

    Link chart with data

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ChartConfiguration, ChartType } from 'chart.js';
-import { Observable, filter, map, of, tap } from 'rxjs';
+import { Observable, map, of } from 'rxjs';
 import { Olympic } from 'src/app/core/models/Olympic';
-import { Participation } from 'src/app/core/models/Participation';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 
 @Component({
@@ -12,17 +11,13 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
 })
 export class HomeComponent implements OnInit {
   public olympics$: Observable<Olympic[]> = of([])
-  // public countries: String[] = []
+  public countries$!: Observable<string[]>
   public medalsNumber: number[] = []
 
   public pieChartOptions: ChartConfiguration['options'] = {
     responsive: true,
   };
   public pieChartType: ChartType = 'pie';
-
-  public countries$: Observable<String[]> = of([])
-
-  
 
   constructor(private olympicService: OlympicService) {}
 
@@ -32,29 +27,13 @@ export class HomeComponent implements OnInit {
       map(data => data.map(item => new Olympic(item.id, item.country, item.participations)))
     )
 
-    
-
-    // this.olympics$.subscribe(item => {
-    //   item.forEach(value => {
-    //     this.countries.push(value.country)
-    //   })
-    //   item.forEach(value => {
-    //     let medals = 0
-    //     value.participations.forEach(participation => {
-    //       medals += participation.medalsCount
-    //     })
-    //     this.medalsNumber.push(medals)
-    //   })
-    //   console.log(this.medalsNumber)
-    // })
-    // console.log(this.medalsNumber)
-
     this.olympics$.subscribe(item => {
-      let countries: String[] = []
+      let countries: string[] = []
       item.forEach(value => {
         countries.push(value.country)
       })
       this.countries$ = of(countries)
+      this.pieChartData.labels = countries
 
       item.forEach(value => {
         let medals = 0
@@ -63,34 +42,18 @@ export class HomeComponent implements OnInit {
         })
         this.medalsNumber.push(medals)
       })
-      // console.log(this.countries$)
-      console.log(this.medalsNumber)
       this.pieChartData.datasets.forEach(value => {
         value.data = this.medalsNumber
       })
     })
-    // console.log(this.medalsNumber)
   }
 
-  // getLabels(): String[] {
-  //   return this.countries
-  // }
-
-  getMedals(): number[] {
-    return this.medalsNumber
-  }
-
-  public pieChartData = 
-  {
-    // labels: ["Italy", "France", "Germany", "Spain", "United State"],
-    // labels: ,
+  public pieChartData = {
+    labels: ["One", "Two", "Three", "Four", "Five"],
     datasets: [{
       data: [1, 2, 3, 4, 5],
-      // data: this.getMedals(),
       backgroundColor: ['red', 'orange', 'yellow', 'green', 'brown'],
       hoverBackgroundColor: ['darkred', 'darkorange', 'darkyellow', 'darkgreen', 'darkbrown']
     }]
   };
-
-  
 }
