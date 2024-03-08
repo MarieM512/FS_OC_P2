f27b420d56210c32bf516624267942ac7fe70d0b
Marie Metay <mariemetay.pro@gmail.com>
2024-03-08 12:05:50 +0100

    Modify header to add back button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { take } from 'rxjs';
 import { OlympicService } from './core/services/olympic.service';
+import { IsActiveMatchOptions, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,9 +9,21 @@ import { OlympicService } from './core/services/olympic.service';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
+  private router = inject(Router);
+
   constructor(private olympicService: OlympicService) {}
 
   ngOnInit(): void {
     this.olympicService.loadInitialData().pipe(take(1)).subscribe();
+  }
+
+  isRouteActive(url: string): boolean {
+    const matchOptions: IsActiveMatchOptions = {
+      paths: 'subset',
+      queryParams: 'subset',
+      fragment: 'ignored',
+      matrixParams: 'ignored'
+    };
+    return this.router.isActive(url, matchOptions);
   }
 }
